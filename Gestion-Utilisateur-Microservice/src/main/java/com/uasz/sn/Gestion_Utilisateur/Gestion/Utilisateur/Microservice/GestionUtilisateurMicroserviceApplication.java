45aae064ad5a4846e503ef7149eb96e7aadd4316
ablayefaye04 <fayeablaye404@gmail.com>
2025-01-31 22:06:00 +0000

    ajouter du projet Utilisateur BACK END

---
+++ Gestion-Utilisateur-Microservice/src/main/java/com/uasz/sn/Gestion_Utilisateur/Gestion/Utilisateur/Microservice/GestionUtilisateurMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.uasz.sn.Gestion_Utilisateur.Gestion.Utilisateur.Microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionUtilisateurMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionUtilisateurMicroserviceApplication.class, args);
+	}
+
+}
---
+++ Gestion-Utilisateur-Microservice/src/test/java/com/uasz/sn/Gestion_Utilisateur/Gestion/Utilisateur/Microservice/GestionUtilisateurMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uasz.sn.Gestion_Utilisateur.Gestion.Utilisateur.Microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionUtilisateurMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
